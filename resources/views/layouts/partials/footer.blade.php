3630dfa5d38de365bc2f791e8e9b8bde0a91a400
sencere <morpheusspirit003@gmail.com>
2022-05-06 22:55:27 +0200

    add license

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="page-footer font-small pt-3">
+<footer class="page-footer font-small pt-3 fixed-bottom">
   <div class="container-fluid text-center text-md-left">
     <div class="row">
         <div class="col-md-6 mt-md-0 mt-3">
-        <h5 class="text-uppercase">TESTING PLATFORM</h5>
-        <p>This site is created only for testing purposes.</p>
+            <h5 class="text-uppercase"></h5>
+            <p></p>
         </div>
       <hr class="clearfix w-100 d-md-none pb-3">
     </div>
